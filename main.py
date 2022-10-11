c2ed052780cedde1a1f9d76aedfeaca1fc7c112b
Alexey Alisin <akalisin@mai.education>
2022-10-11 22:13:19 +0300

    HAHAHAHAHAHHA

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from numerical import runge_kutta4
+from matplotlib import pyplot as plt
+import math
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    def func(x, t):
+        return t * math.sqrt(x)
 
+    ts, xs = runge_kutta4(func, h=0.1, t_bound=10.1, t0=0, x0=1)
+    _, ys = runge_kutta4(func, h=0.1, t_bound=10.1, t0=0, x0=1)
+    _, zs = runge_kutta4(func, h=0.1, t_bound=10.1, t0=0, x0=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    figure, axis = plt.subplots(1, 3)
+
+    plt.plot(ts, xs, color="red",  linestyle='solid')
+    plt.ylabel("x(t)")
+    plt.xlabel("t")
+    plt.show()
+
--- numerical.py
+++ numerical.py
@@ -6,7 +6,7 @@ def runge_kutta4(func, h, t_bound, t0, x0):
     xs = [x0]
     ts = np.arange(t0, t_bound, h)
 
-    for t in ts:
+    for t in ts[0:-1]:
         k1 = func(x_i, t)
         k2 = func(x_i + h / 2 * k1, t + h / 2)
         k3 = func(x_i + h / 2 * k2, t + h / 2)
@@ -14,6 +14,8 @@ def runge_kutta4(func, h, t_bound, t0, x0):
         x = x_i + (k1 + 2 * k2 + 2 * k3 + k4) / 6 * h
         xs.append(x)
         x_i = x
+
+
 
     return ts.tolist(), xs
 
